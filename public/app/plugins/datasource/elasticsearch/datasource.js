c54a056f30ffa31ddea659a1abab1d89c97a9bd9
Naved Alam <alam.naved18@gmail.com>
2016-10-25 19:43:46 +0530

    Fixed query bug

--- public/app/plugins/datasource/elasticsearch/datasource.js
+++ public/app/plugins/datasource/elasticsearch/datasource.js
@@ -207,8 +207,8 @@ function (angular, _, moment, kbn, ElasticQueryBuilder, IndexPattern, ElasticRes
         var luceneQuery = angular.toJson(target.query || '*');
         // remove inner quotes
         luceneQuery = luceneQuery.substr(1, luceneQuery.length - 2);
-        luceneQuery = templateSrv.replace(luceneQuery, options.scopedVars);
         luceneQuery = luceneQuery.replace(" and ", " AND ").replace(" or "," OR ").replace(" not "," NOT ");
+        luceneQuery = templateSrv.replace(luceneQuery, options.scopedVars);
         luceneQuery = luceneQuery.replace(new RegExp("[AND |OR |OR NOT |AND NOT ]*[A-Za-z_0-9]*:a123a","gm"),"");
         luceneQuery = luceneQuery.trim();
         if(luceneQuery.startsWith('AND') || luceneQuery.startsWith("OR")){
