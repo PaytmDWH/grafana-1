061a6b99882e9bb896ac8e24a8fbec77642c63a1
ankitpal <ankit.pal@paytm.com>
2017-04-12 17:21:19 +0530

    quickfix sort

--- public/app/core/table_model.ts
+++ public/app/core/table_model.ts
@@ -22,11 +22,11 @@ export default class TableModel {
       if (options.dev===undefined || options.dev===false) {
         var devindex = 0;
       }
-      
-      if (a === undefined || isNaN(a)) {
+
+      if (a === undefined) {
         return -1;
       }
-      if (b === undefined || isNaN(b)) {
+      if (b === undefined) {
         return 1;
       }
 
